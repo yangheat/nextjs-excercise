b0893bd1975b084a295bbcba58e981969fe3ae68
SunPark <63341343+yangheat@users.noreply.github.com>
2024-02-07 12:07:56 +0900

    Delete nextjs-blog directory

--- nextjs-blog/next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://nextjs-blog-yangheat.vercel.app/',
-  generateRobotsTxt: true, // (optional)
-  // ...other options
-  exclude: ['/api/*'],
-}
